d37fa8555de17386a3f5c177fa969a5a1ead1739
CafeeWWW <brunocaneo3@gmail.com>
2021-06-30 13:37:48 -0300

    first commit

---
+++ next-seo.config.js
@@ -0,0 +1,16 @@
+export default {
+    description: "Movels Part's, empresa de pés e puxadores para móveis de cozinha.",
+    openGraph: {
+        type: 'website',
+        locale: 'pt_BR',
+        url: 'https://movelparts.com.br/',
+        description: "Movels Part's, empresa de pés e puxadores para móveis de cozinha.",
+        site_name: "Movel Part's",
+    },
+    images: [
+        {
+        url: '/logo-text.svg',
+        alt: "Logo da Movel Part's",
+        }
+    ]
+}
